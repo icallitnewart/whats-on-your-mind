549527bb0e30960c0c638ab71bf5dc88a2650af7
icallitnewart <icallitnewart@gmail.com>
2023-04-07 02:27:03 +0900

    Fix: 원치 않는 경로로 진입시 리다이렉트 되도록 수정

--- src/core/router.js
+++ src/core/router.js
@@ -1,11 +1,14 @@
 import locationStore from "../store/location";
 
 function routeRender(routes) {
-  const routerView = document.querySelector('router-view');
+  //다른 경로로 진입 방지
+  const path = window.location.pathname;
+  if (path !== '/') window.location.replace('/');
 
   //라우트 정보에 맞게 컴포넌트 렌더링
   const currentRoute = routes.find(route => route.page === locationStore.state.currentPage);
 
+  const routerView = document.querySelector('router-view');
   routerView.innerHTML = '';
   routerView.append(new currentRoute.component().element);
 }
